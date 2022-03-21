8b9a8ad191ac6cc1d053c9408ecedc751603e76d
Mikhail Firstov <cyber-punk@xakep.ru>
2022-03-21 16:28:49 +0300

    Update broadcast.sh

--- videostream/broadcast.sh
+++ videostream/broadcast.sh
@@ -2,6 +2,8 @@
 
 source /etc/container_environment.sh
 
+while :; do
+
 ffmpeg \
     -re -f lavfi -i "movie=filename=${VS_VIDEO_SOURCE}:loop=0, setpts=N/(FRAME_RATE*TB)" \
     -thread_queue_size 512 -i "${VS_AUDIO_SOURCE}" \
@@ -11,5 +13,8 @@ ffmpeg \
      box=${VS_TEXT_BOX}: boxcolor=${VS_TEXT_BOX_COLOR}: boxborderw=${VS_TEXT_BORDER_W}: \
      textfile=${VS_TEXT_SOURCE}: reload=1: fontcolor=white@0.8: x=${VS_TEXT_X}: y=${VS_TEXT_Y}" \
     -vcodec libx264 -pix_fmt yuv420p -preset ${VS_QUALITY} -r ${VS_FPS} -g $((${VS_FPS} * 2)) -b:v ${VS_VBR} \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+    -acodec aac -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
     -f flv "${VS_RTMP_SERVER}"
+sleep 1;
+echo "Reconnecting..."
+done
